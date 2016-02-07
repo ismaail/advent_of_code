88a336178eb61f5561bdbc377f297914f055c04d
E.G. ismaail <contact@ismaail.com>
2016-02-07 21:00:34 +0000

    Add Play class

---
+++ src/Day3/Play.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Puzzle\Day3;
+
+use Puzzle\Playable;
+
+/**
+ * Class Play
+ * @package Puzzle\Day3
+ */
+class Play implements Playable
+{
+    public function play()
+    {
+        $area = new Area();
+        $santa = new Santa();
+
+        $parser = new InstructionParser($area, $santa);
+
+        $file = '/../../data/instructions/day3.dat';
+
+        $parser->parse(file_get_contents(__DIR__ . $file));
+
+        return sprintf('Total houses visited by Santa: %d house', count($area));
+    }
+}
